c49c6c6e016aa19a0a09a4df9eff25fe9fb2c5ad
Ferdinand Mbecha <75342805+Awungferd@users.noreply.github.com>
2021-06-09 17:05:40 +0200

    ok
    Just me wishing upon a coded star

--- trial.js
+++ trial.js
@@ -23,3 +23,29 @@ for (let index = 0; index < animals.length; index++) {
     // } else{return template.type= "Shake it more"}
 
     }
+
+    const charsTemp= {};
+ function highestFreq(arr) {
+for (let iterator = 0; iterator < events.length; iterator++) {
+    const element = events[iterator];
+    strOnCount=element.remark
+   const arr= strOnCount.split(" ");
+
+    let moFre=arr[0], maxRekon=0,i,j;
+    let len=arr.length;
+    for(i=0;i<len;i++){let count=0;
+    for(j=i+1;j<len;j++){
+        if(arr[i]==arr[j]){
+            count++;
+           
+        }
+    }
+    if(maxRekon<count){
+        maxRekon=count;
+        moFre=arr[i];
+        
+    }
+    }
+    console.log (moFre);
+}}
+console.log(`The word with the highest frequency is: ${highestFreq()}`)
